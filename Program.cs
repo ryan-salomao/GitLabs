4a8160ae0b5603100d58bceaf4220db4826620d4
ryan-salomao <ryansalomao@gmail.com>
2022-05-31 16:50:20 -0300

    operacao Somar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var operacao = args[0];
+var n1 = Convert.ToDouble(args[1]);
+var n2 = Convert.ToDouble(args[2]);;
+
+if (operacao == "Somar")
+{
+    Console.WriteLine(n1+n2);
+}
